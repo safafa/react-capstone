1f43767d60bd55fd97853415b41f0b59da13fc3d
unknown <safaaballagh97@gmail.com>
2021-10-13 22:24:19 +0100

    Fix App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/ConfigureStore';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('Snapshot test', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>,
+    )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
