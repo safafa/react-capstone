f16e43d00153309907d7d4fb0d87497c2bffa65a
unknown <safaaballagh97@gmail.com>
2021-10-13 14:12:10 +0100

    Add arrow icon

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { BsArrowRightCircle } from 'react-icons/bs';
 
 const Country = ({ country }) => {
   const page = `/details/${country.id}`;
   return (
     <a className="w-50 text-decoration-none link-dark text-center" href={page}>
       <Card>
-        <Card.Title>{country.name}</Card.Title>
+        <div className="d-flex justify-content-between align-items-center px-3 pt-2">
+          <Card.Title>{country.name}</Card.Title>
+          <BsArrowRightCircle />
+        </div>
         <Card.Body>
           <Card.Text>
             {country.today_confirmed}
