1664d40fa6f599ecaa01e4001b20cb279e8c504d
unknown <safaaballagh97@gmail.com>
2021-10-14 19:32:19 +0100

    Remove comment from detail

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
